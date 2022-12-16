3623eb50242056aedb391c9c00726f558964bc63
Daisy-Violet <a.laska.yang@mail.ru>
2022-12-17 00:22:33 +0300

    Add new project

---
+++ index.js
@@ -0,0 +1,8 @@
+let firstImg = document.getElementById('firstImg');
+
+function nextImg() {
+    firstImg.src = "./assets/images/2.jpg";
+}
+function turnBack() {
+    firstImg.src = "./assets/images/1.jpg";
+}
